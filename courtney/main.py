02e9f19ff5a6eb2ba39ae1749278d4bb566c5444
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-16 15:38:14 -0300

    Saved courtney/main.py at Timestamp: 2018-04-16 18:38:14

--- courtney/main.py
+++ courtney/main.py
@@ -1,1 +1,16 @@
-# grete
+# grete.courtney.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto
+wardrobe = "https://i.imgur.com/FmhekcD.jpg"
+toy = "https://i.imgur.com/57cOaZ9.jpg"
+sckoolhouse = "https://i.imgur.com/oXsdN2c.jpg"
+
+def claudemilsonarevolta():
+    cenasckoolhouse = Cena("https://i.imgur.com/oXsdN2c.jpg")
+    wardrobe = Elemento ("https://i.imgur.com/FmhekcD.jpg", tit = "toy", style = dict(left = 150, top = 60, widht = 60, height = 200))
+    txtwardrobe = (cenasckoolhouse,"Become Claudemilson")
+    cenasckoolhouse.vai()
+    wardrobe.entra(cenasckoolhouse)
+    
+    
+claudemilsonarevolta()
+
