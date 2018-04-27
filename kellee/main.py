7ba83fdc87da8000504c6049560e0eac4ac22fd6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-27 14:47:29 -0300

    Created kellee/main.py at Timestamp: 2018-04-27 17:47:29

---
+++ kellee/main.py
@@ -0,0 +1,53 @@
+# grete.kellee.main.py
+from _spy.vitollino.main import Cena, Texto, Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+
+
+A_NORTE = "https://i.imgur.com/aLEjWgB.png"
+A_LESTE = "https://i.imgur.com/sivjAnO.png"
+A_SUL = "https://i.imgur.com/otHJhF0.png"
+
+B_NORTE = "https://i.imgur.com/40K5493.png"
+B_LESTE = "https://i.imgur.com/R3bpFXD.png"
+B_OESTE = "https://i.imgur.com/dlxY8hi.png"
+B_SUL = "https://i.imgur.com/eYM3Yp9.png"
+
+C_LESTE = "https://i.imgur.com/94V79TA.png"
+C_NORTE = "https://i.imgur.com/YJfnhy9.png"
+C_OESTE = "https://i.imgur.com/Fzz2FNz.png"
+C_SUL = "https://i.imgur.com/LFKXlB1.png"
+
+D_NORTE = "http://i.imgur.com/1uWH7rU.png"
+D_LESTE = "https://i.imgur.com/b0FcjLq.png"
+D_OESTE = "https://i.imgur.com/406g75C.png"
+D_SUL = "https://i.imgur.com/HQBtUoQ.png"
+
+H_NORTE = "https://i.imgur.com/WjTtZPn.png"
+H_LESTE = "https://i.imgur.com/AzvB8hs.png"
+H_OESTE = "https://i.imgur.com/SIhLGCP.png"
+H_SUL = "https://i.imgur.com/UVnpzzE.png"
+
+I_NORTE= "https://i.imgur.com/RSdQSH1.png"
+I_SUL = "https://i.imgur.com/UGCRJ0d.png"
+I_LESTE = "https://i.imgur.com/jSn4zsl.png"
+I_OESTE= "https://i.imgur.com/eG43vn5.png"
+
+J_NORTE= "https://i.imgur.com/MMO11Dv.png"
+J_SUL = "https://i.imgur.com/RkWPb8Z.png"
+J_LESTE = "https://i.imgur.com/btv0qfO.png"
+J_OESTE= "https://i.imgur.com/lDezYKu.png"
+
+E_NORTE= "https://i.imgur.com/uNkTVGg.png"
+E_SUL = "http://i.imgur.com/bculg4O.png"
+E_LESTE = "https://i.imgur.com/lUi1E1v.png"
+E_OESTE = "https://i.imgur.com/bPBT1d7.png"
+
+K_NORTE= "https://i.imgur.com/Tx9Q6vW.png"
+K_SUL = "https://i.imgur.com/rrI94Xh.png"
+K_LESTE = "https://i.imgur.com/R6gON2E.png"
+K_OESTE= "https://i.imgur.com/Mn69uua.png"
+
+L_NORTE= "https://i.imgur.com/oAu9lkN.png"
+L_SUL = "https://i.imgur.com/xTjd7UV.png"
+L_LESTE = "https://i.imgur.com/JMQAGvc.png"
+L_OESTE = "http://i.imgur.com/UJBMKY7.png"
