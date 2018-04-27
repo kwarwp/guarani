d18b42e35710611bec9dd58b8f22cad978df9fb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-27 15:36:27 -0300

    Saved roxanne/main.py at Timestamp: 2018-04-27 18:36:27

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,7 +1,7 @@
 from _spy.vittolino.main import Cena, Elemento, Texto
 
 #http://www.portaldoscaesegatos.com.br/wp-content/uploads/2016/07/Animais-loucos-1.jpg                                
-Thedoglink = http://videosdehumor.esy.es/wp-content/uploads/2016/01/caes-e-gatos-engracado-scared-da-520x245.jpg
+thedoglink = "http://videosdehumor.esy.es/wp-content/uploads/2016/01/caes-e-gatos-engracado-scared-da-520x245.jpg"
   
 
 
