5925013acc45c439554151c98d0351246d701664
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-27 15:28:05 -0300

    Saved courtney/main.py at Timestamp: 2018-04-27 18:28:04

--- courtney/main.py
+++ courtney/main.py
@@ -1,15 +1,15 @@
 # grete.courtney.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto
-wardrobe = "https://i.imgur.com/FmhekcD.jpg"
+children = "https://i.imgur.com/4fTrn8X.jpg"
 toy = "https://i.imgur.com/57cOaZ9.jpg"
 sckoolhouse = "https://i.imgur.com/oXsdN2c.jpg"
 
 def claudemilsonarevolta():
     cenasckoolhouse = Cena("https://i.imgur.com/oXsdN2c.jpg")
-    wardrobe = Elemento ("https://i.imgur.com/FmhekcD.jpg", tit = "wardrobe", style = dict(left = 150, top = 60, widht = 60, height = 200))
-    txtwardrobe = Texto(cenasckoolhouse,"please, help me")
-    wardrobe.entra(cenasckoolhouse)
-    wardrobe.vai = txtwardrobe.vai
+    children = Elemento ("https://i.imgur.com/4fTrn8X.jpg", tit = "children", style = dict(left = 150, top = 60, widht = 60, height = 200))
+    txtchildren = Texto(cenasckoolhouse,"please, help me")
+    children.entra(cenasckoolhouse)
+    children.vai = txtchildren.vai
     cenasckoolhouse.vai()
     
     
