a974d1b1dcf5d656d6da3b6dcdda1d36df3901af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-29 18:10:50 -0300

    Saved meredith/main.py at Timestamp: 2018-04-29 21:10:50

--- meredith/main.py
+++ meredith/main.py
@@ -58,10 +58,6 @@ PANO = "http://www.removermanchas.net/wp-content/uploads/2012/11/Remover-nodoa-s
 
 def criarsalas():
  a_norte = Cena(img=A_NORTE)
- a_leste = Cena(img=A_LESTE, esquerda=a_norte)
- a_sul = Cena(img=A_SUL, esquerda=a_leste)
- a_norte.direita = a_leste
- a_leste.direita = a_sul
  b_leste = Cena(img=B_LESTE)
  a_leste.meio = b_leste
     
