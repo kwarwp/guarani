66afbbbf820c85ce5774fdedd786f3bc15d91c2a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-04 15:46:17 -0300

    Saved sara/main.py at Timestamp: 2018-05-04 18:46:17

--- sara/main.py
+++ sara/main.py
@@ -1,3 +1,15 @@
 # grete.sara.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto
-sckoolhouseentrance = "https://i.imgur.com/yAlpsYW.jpg
+BEACH = "https://www.guiaviagensbrasil.com/imagens/bela-praia-camboinha-joao-pessoa-p"
+FUNNYDOG = "http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg"
+
+def dogatthebeach():
+    beach = Cena ("https://www.guiaviagensbrasil.com/imagens/bela-praia-camboinha-joao-pessoa-p")
+    funnydog = Elemento("http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg", tit = "FUNNYDOG",  style = dict(left = 150, top = 60, widht = 60, height = 200))
+    txtfunnydog = Texto(cenabeach,"do you need dolly?")
+    txtfunnydog.entra(cenabeach)
+    funnydog.vai = txtfunnydog.vai
+    cenabeach.vai()
+    
+    
+dogatthebeach()
