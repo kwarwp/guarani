3b4035e194baba3af923cd1d4d52dd793cfe51ba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-04 15:41:30 -0300

    Saved libby/main.py at Timestamp: 2018-05-04 18:41:30

--- libby/main.py
+++ libby/main.py
@@ -1,1 +1,15 @@
 # grete.libby.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto
+CAMPO = "https://image.freepik.com/fotos-gratis/paisagem-de-campo-de-golfe_1388-96.jpg"
+CASAL = "https://www.temporadalivre.com/blog/wp-content/uploads/casal-sem-crise-10-dicas-que-o-casal-necessita-para-ser-feliz.jpg"
+
+def boyfriendsatthecamp():
+    campo = Cena (img = CAMPO)
+    casal = Elemento(img = CASAL, tit = "casal", style = dict(left = 150, top = 60, height = 200))
+    casal_t = Texto(casal,"let's eat something!")
+    casal_t.entra(campo)
+    casal.vai = dog_t.vai
+    campo.vai()
+    
+    
+boyfriendsatthecamp()
