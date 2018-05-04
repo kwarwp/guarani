0c7c950b87813ef1da47cd8a274f3a66e9b5806b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-04 15:20:26 -0300

    Saved libby/main.py at Timestamp: 2018-05-04 18:20:26

--- libby/main.py
+++ libby/main.py
@@ -1,1 +1,5 @@
 # grete.libby.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto
+BEACH = "https://www.guiaviagensbrasil.com/imagens/bela-praia-camboinha-joao-pessoa-p"
+FUNNYDOG = "http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg"
+
