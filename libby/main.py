99be244a13cb9ae3f9b88a4376745bea5de3c36c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-04 15:31:58 -0300

    Saved libby/main.py at Timestamp: 2018-05-04 18:31:58

--- libby/main.py
+++ libby/main.py
@@ -3,5 +3,6 @@ from _spy.vitollino.main import Cena,Elemento,Texto
 BEACH = "https://www.guiaviagensbrasil.com/imagens/bela-praia-camboinha-joao-pessoa-p"
 FUNNYDOG = "http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg"
 
-def dogatthebeach(): 
-    cenaBEACH = Cena("https://www.guiaviagensbrasil.com/imagens/bela-praia-camboinha-joao-pessoa-p")
+def dogatthebeach():
+    beach = Cena (img = BEACH)
+    funnydog = Elemento(img = FUNNYDOG, tit = "FUNNYDOG",  style = dict(left = 150, top = 60, widht = 60, height = 200))
