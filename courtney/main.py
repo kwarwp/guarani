2ebd8335c7f82935890b99fa85d230e6b5956e87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 14:40:38 -0300

    Saved courtney/main.py at Timestamp: 2018-05-07 17:40:38

--- courtney/main.py
+++ courtney/main.py
@@ -7,7 +7,7 @@ sckoolhouse = "https://i.imgur.com/oXsdN2c.jpg"
 
 def claudemilsonarevolta():
     cenasckoolhouse = Cena("https://i.imgur.com/oXsdN2c.jpg")
-    children = Elemento("https://i.imgur.com/4fTrn8X.jpg", tit = "children", style = dict(left = 825, top = 100, widht = 60, height = 200))
+    children = Elemento("https://i.imgur.com/4fTrn8X.jpg", tit = "children", style = dict(left = 825, top = 10, widht = 60, height = 200))
     txtchildren = Texto(cenasckoolhouse,"please, help me")
     children.entra(cenasckoolhouse)
     children.vai = txtchildren.vai
