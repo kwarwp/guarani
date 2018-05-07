de95707436bdf9d57db667f0ae773523368903f1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 15:12:58 -0300

    Saved stacy/main.py at Timestamp: 2018-05-07 18:12:58

--- stacy/main.py
+++ stacy/main.py
@@ -2,13 +2,13 @@
 # grete.sara.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto
 BEACH = "http://casadeluxoemcamboinhas.com.br/wp-content/uploads/2015/01/28506.jpg"
-FUNNYDOG = "http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg"
+FUNNYDOG = "https://vignette.wikia.nocookie.net/steven-universe/images/0/06/Funny_dog.png/revision/latest?cb=20150717003538"
 DOLLYNHO =  "https://lh3.ggpht.com/vshyoy6DTJtuLpXrqKesDJkJebNmpq7yTI-HUr7ICZ_jOe_xXBEWaGmYDrnbVDZxwA=w300"
 LIXO = "http://portaljfonte.com.br/wp-content/uploads/2017/10/lixoI.jpg"
 
 def dogatthebeach():
     beach = Cena ("http://casadeluxoemcamboinhas.com.br/wp-content/uploads/2015/01/28506.jpg")
-    funnydog = Elemento("http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg", tit = "FUNNYDOG",  style = dict(left = 150, top = 60, widht = 60, height = 200))
+    funnydog = Elemento("https://vignette.wikia.nocookie.net/steven-universe/images/0/06/Funny_dog.png/revision/latest?cb=20150717003538", tit = "FUNNYDOG",  style = dict(left = 150, top = 60, widht = 60, height = 200))
     txtfunnydog = Texto(beach,"Do you need dolly?")
     funnydog.entra(beach)
     funnydog.vai = txtfunnydog.vai
