7fc67a2186d84b3588e2db9974604d0477ef3729
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 14:39:47 -0300

    Saved courtney/main.py at Timestamp: 2018-05-07 17:39:47

--- courtney/main.py
+++ courtney/main.py
@@ -1,13 +1,13 @@
 # grete.courtney.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
-STYLE["width"] = 1000
+STYLE["width"] = 800
 children = "https://i.imgur.com/4fTrn8X.jpg"
 toy = "https://i.imgur.com/57cOaZ9.jpg"
 sckoolhouse = "https://i.imgur.com/oXsdN2c.jpg"
 
 def claudemilsonarevolta():
     cenasckoolhouse = Cena("https://i.imgur.com/oXsdN2c.jpg")
-    children = Elemento("https://i.imgur.com/4fTrn8X.jpg", tit = "children", style = dict(left = 150, top = 60, widht = 60, height = 200))
+    children = Elemento("https://i.imgur.com/4fTrn8X.jpg", tit = "children", style = dict(left = 825, top = 100, widht = 60, height = 200))
     txtchildren = Texto(cenasckoolhouse,"please, help me")
     children.entra(cenasckoolhouse)
     children.vai = txtchildren.vai
