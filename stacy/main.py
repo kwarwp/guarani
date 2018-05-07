03171de69a78ffcb35e18ae6bb3f6e907f692c25
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-07 14:56:47 -0300

    Saved stacy/main.py at Timestamp: 2018-05-07 17:56:47

--- stacy/main.py
+++ stacy/main.py
@@ -3,6 +3,8 @@
 from _spy.vitollino.main import Cena,Elemento,Texto
 BEACH = "http://casadeluxoemcamboinhas.com.br/wp-content/uploads/2015/01/28506.jpg"
 FUNNYDOG = "http://www.instatube.com.br/wp-content/uploads/2015/08/pugs-mais-engracados-do-mundo-vi-480x293.jpg"
+DOLLYNHO =  "https://lh3.ggpht.com/vshyoy6DTJtuLpXrqKesDJkJebNmpq7yTI-HUr7ICZ_jOe_xXBEWaGmYDrnbVDZxwA=w300"
+LIXO = "http://portaljfonte.com.br/wp-content/uploads/2017/10/lixoI.jpg"
 
 def dogatthebeach():
     beach = Cena ("http://casadeluxoemcamboinhas.com.br/wp-content/uploads/2015/01/28506.jpg")
@@ -11,6 +13,8 @@ def dogatthebeach():
     funnydog.entra(beach)
     funnydog.vai = txtfunnydog.vai
     beach.vai()
+    lixo = Cena ("http://portaljfonte.com.br/wp-content/uploads/2017/10/lixoI.jpg")
+    dollynho = Elemento("https://lh3.ggpht.com/vshyoy6DTJtuLpXrqKesDJkJebNmpq7yTI-HUr7ICZ_jOe_xXBEWaGmYDrnbVDZxwA=w300")
     
     
 dogatthebeach()
