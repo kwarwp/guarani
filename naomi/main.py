65b927465a4bb9c2702b4c3b97c522a3fbfb563a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-14 15:11:22 -0300

    Created naomi/main.py at Timestamp: 2018-05-14 18:11:22

---
+++ naomi/main.py
@@ -0,0 +1,34 @@
+# grete.naomi.main.py
+from _spy.vitollino.main import Cena, Elemento, Texto, INVENTARIO, STYLE
+STYLE["width"] = 800
+STYLE["height"] = "600px"
+MIC = "https://i.imgur.com/iqUV0yJ.jpg"
+PAN = "https://i.imgur.com/NFhoyd0.jpg"
+NGEO = "https://i.imgur.com/VzxARws.jpg"
+LGEO = "https://i.imgur.com/zb8RS7U.jpg"
+SGEO = "https://i.imgur.com/iybLHJK.jpg"
+OGEO = "https://i.imgur.com/js589HB.jpg"
+
+def geografia():
+    INVENTARIO.inicia()
+    n_geo = Cena(NGEO)
+    e_geo = Cena(LGEO, esquerda=n_geo)
+    s_geo = Cena(SGEO, esquerda=e_geo)
+    o_geo = Cena(OGEO, esquerda=s_geo, direita=n_geo)
+    n_geo.esquerda, n_geo.direita = o_geo, e_geo
+    s_geo.direita, e_geo.direita = o_geo, s_geo
+    micstyle = dict(left = 610, top = 100, width = 80, maxHeight = "90px")
+    panstyle = dict(left = 750, top = 110, width = 50, maxHeight = "230px")
+    volcstyle = dict(left = 30, top = 500, width = 100, maxHeight = "120px")
+    mic = Elemento(MIC, tit = "sweep pan", drag=True,
+        style=micstyle, cena=s_geo, vai=Texto(s_geo,"please, help me, fix my name",
+        foi=lambda: INVENTARIO.bota(mic)).vai)
+    pan = Elemento(PAN, tit = "microscope", drag=True,
+        style=panstyle, cena=e_geo, vai=Texto(e_geo,"please, help me, fix my name",
+        foi=lambda: INVENTARIO.bota(pan)).vai)
+
+    e_geo.vai()
+    
+
+if __name__ == "__main__":
+    geografia()
