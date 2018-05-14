2ce12e0436a3d108704e7968ac18d4d4085fa74a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-14 15:44:39 -0300

    Saved danae/main.py at Timestamp: 2018-05-14 18:44:39

--- danae/main.py
+++ danae/main.py
@@ -2,7 +2,9 @@
 from spy.vitollino.main import Cena,Elemento,Texto,STYLE,Dragger, Droppable, INVENTARIO
 STYLE["width"] = 800
 STYLE["height"] = "600px"
-port_geog_ = "https://i.imgur.com/ilCZrUL.jpg"
-port_geog = "https://i.imgur.com/69t1OYy.jpg"
-port_geog = "https://i.imgur.com/iFENyGO.jpg"
+port_geog_l = "https://i.imgur.com/ilCZrUL.jpg"
+port_geog_n = "https://i.imgur.com/69t1OYy.jpg"
+port_geog_o = "https://i.imgur.com/iFENyGO.jpg"
 port_geog_s = "https://i.imgur.com/kuFcATM.jpg"
+expositor = "https://i.imgur.com/Dpyh2Ag.jpg?s=wa"
+
