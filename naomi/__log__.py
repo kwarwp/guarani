2da0722cf5ef104c26293fdd6183b0a91bd858ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-28 14:29:15 -0300

    Saved naomi/__log__.py at Timestamp: 2018-05-28 17:29:15

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -6,3 +6,25 @@
                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 28 2018 14:29:11.156 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 195
+    geografia()
+  module <module> line 177
+    mic = Elemento(MIC, tit = "sweep pan", drag=False, drop="microscope",
+  module <module> line 20
+    self.img, self.tit = img, tit
+  module <module> line 82
+    self.elt.title = texto
+AttributeError: 'Elemento' object has no attribute 'elt'
+'''},
