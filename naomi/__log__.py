3f7c9e8e5aa292d01656553fe8bed47661665cba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-28 14:16:20 -0300

    Created naomi/__log__.py at Timestamp: 2018-05-28 17:16:20

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon May 28 2018 14:16:15.76 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 175
+  mic = Elemento(MIC, tit = "sweep pan", drag=False, drop="microscope"
+                                                                                ^
+SyntaxError: invalid syntax
+'''},
