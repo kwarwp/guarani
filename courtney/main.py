ef438fd4c18aed58eaa6e70ebe73194700c13b63
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-28 15:42:57 -0300

    Saved courtney/main.py at Timestamp: 2018-05-28 18:42:56

--- courtney/main.py
+++ courtney/main.py
@@ -3,15 +3,20 @@ from _spy.vitollino.main import Cena,Elemento,Texto,STYLE,Dragger, Droppable, IN
 STYLE["width"] = 800
 STYLE["height"] = "600px"
 children = "https://i.imgur.com/4fTrn8X.jpg"
-sckoolhouse = "https://i.imgur.com/oXsdN2c.jpg"
+schoolhouse_n = "https://i.imgur.com/F8vDljG.jpg"
+schoolhouse_o = "https://i.imgur.com/VvB1JWN.jpg
+schoolhouse_s = "https://i.imgur.com/A0CmC45.jpg"
+schoolhouse_l = "https://i.imgur.com/wu3DN2C.jpg
 
 def claudemilsonarevolta():
-    cenasckoolhouse = Cena("https://i.imgur.com/oXsdN2c.jpg")
-    children = Elemento("https://i.imgur.com/4fTrn8X.jpg",tit = "children", style = dict(left = 800, top = 0, width = 352, maxHeight = 100))
-    txtchildren = Texto(cenasckoolhouse,"please, help me")
-    children.entra(cenasckoolhouse)
-    children.vai = txtchildren.vai
-    cenasckoolhouse.vai()
-    
-    
-claudemilsonarevolta()
+    def vai_sckoolhouse
+        from naomi.main import geografia
+        geografia()
+        INVENTARIO.inicia()
+    n_schoolhouse = Cena(sckoolhouse_n)
+    e_schoolhouse = Cena(sckoolhouse_e, esquerda=n_sckoolhouse)
+    s_schoolhouse = Cena(sckoolhouse_s, esquerda=e_sckoolhouse, meio=Cena(vai=vai_geo))
+    o_sckoolhouse = Cena(sckoolhouse_o, esquerda=s_sckoolhouse, direita=n_sckoolhouse)
+    n_sckoolhouse.esquerda, n_sckoolhouse.direita = o_sckoolhouse, e_sckoolhouse
+    s_sckoolhouse.direita,e_sckoolhouse.direita = o_sckoolhouse, s_sckoolhouse
+    from naomi.main import Elemento
