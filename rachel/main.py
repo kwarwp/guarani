b48bc9de2dc7ce86b07e9e329c9e8670c4f0a485
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-05-28 15:43:30 -0300

    Saved rachel/main.py at Timestamp: 2018-05-28 18:43:30

--- rachel/main.py
+++ rachel/main.py
@@ -1,1 +1,6 @@
 # grete.rachel.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto
+SECRETARIA1 = "http://imgur.com/gallery/88uA2AT"
+SECRETARIA2 = "https://m.imgur.com/gallery/5Y2xnJW"
+SECRETARIA3 = "https://m.imgur.com/a/mDqZs12?s=wa"
+SECRETARIA4 = "https://imgur.com/a/f6PLbCo?s-wa"
