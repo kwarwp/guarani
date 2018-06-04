f517d79b2f6728b3e9292e5f4c4d464a628cf1b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-04 14:52:21 -0300

    Created stacy/__log__.py at Timestamp: 2018-06-04 17:52:20

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Jun 04 2018 14:52:17.807 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  INVENTARIO.inicia()
+  ^
+IndentationError: unexpected indent
+'''},
