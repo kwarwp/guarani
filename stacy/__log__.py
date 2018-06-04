a4f9878a075c1b422f7d452a9413edfd10dca8a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-06-04 14:54:15 -0300

    Saved stacy/__log__.py at Timestamp: 2018-06-04 17:54:15

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Jun 04 2018 14:54:14.852 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  def sala()
+             ^
+SyntaxError: invalid syntax
+'''},
